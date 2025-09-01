87d82fdad7a066c63ecbafdc66b698d3fa94c98a
MaksGrn <maksymilian.gronowski@gmail.com>
2025-09-01 08:55:01 +0200

    Delete src directory

--- src/util/tileLayer.js
+++
@@ -1,7 +0,0 @@
-const tileLayer = {
-  url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-};
-
-export default tileLayer;
